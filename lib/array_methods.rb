71db990400825d56266f75307a18237f4208b943
Daniel Siekawitch <daniel.siekawitch@gmail.com>
2019-07-14 02:56:52 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -4,7 +4,7 @@ def find_element_index(array, value_to_find)
   while counter << scale.include?() 
      counter += 1
   end
-  value_to_find.to_i
+  value_to_find.to_i 
 end
 
 def find_max_value(array)
