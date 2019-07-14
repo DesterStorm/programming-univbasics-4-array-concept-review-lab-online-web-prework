57d52152449096eb3316437abf0be9974ce19266
Daniel Siekawitch <daniel.siekawitch@gmail.com>
2019-07-14 03:34:10 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,8 +1,6 @@
 def find_element_index(scale, value_to_find)
   counter = 0 
-  while counter <= scale[2] do 
-    puts counter
-     counter += 1 
+  
   end
  value_to_find = counter
  value_to_find
